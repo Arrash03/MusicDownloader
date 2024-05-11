bba6abb2b647d81358083cc3ee0fb82039f4479b
Arrash03 <atv1382@gmail.com>
2024-05-11 22:10:01 +0330

    sjadf;

--- biaMusic.ts
+++ biaMusic.ts
@@ -15,6 +15,11 @@ interface IMusicUrls {
     album_musics: IAlbum[]
 }
 
+export enum Quality {
+    _320,
+    _128
+}
+
 export default class BiaMusic {
     // static URL: string = "https://biamusic.ir";
     static BROWSER_CONFIGS: PuppeteerLaunchOptions = {
@@ -23,18 +28,21 @@ export default class BiaMusic {
         headless: false
     };
     static GO_TO_OPTIONS: GoToOptions = {
-        waitUntil: "domcontentloaded",
+        waitUntil: "load",
         // could place in an env file
-        timeout: 30000
+        // it need to be base on page numbers and connection speed
+        timeout: 20000
     };
+    static MAX_OPEN_TAB: number = 5;
     static browser: Browser;
     url: string;
     musicUrls: IMusicUrls;
     singerName: string;
     limit: number;
     page: null | Page;
+    quality: Quality;
 
-    constructor(singerName: string, url: string, limit: number = 0) {
+    constructor(singerName: string, url: string, quality: Quality, limit: number = 0) {
         this.url = url;
         this.singerName = singerName;
         this.limit = Math.abs(limit);
@@ -43,6 +51,7 @@ export default class BiaMusic {
             album_musics: []
         };
         this.page = null;
+        this.quality = quality;
     }
 
     private async createBrowser() {
@@ -54,31 +63,67 @@ export default class BiaMusic {
     public async find() {
         await this.createBrowser();
         await this.page!.goto(this.url, BiaMusic.GO_TO_OPTIONS);
-        await this.find_single_musics();
+        await this.findSingleMusics();
 
-        console.log(this.musicUrls.single_musics);
+        console.log(this.musicUrls.single_musics, this.musicUrls.single_musics.length);
 
 
     }
 
-    private async find_single_musics() {
+    private async findSingleMusics() {
         const SELECTOR = "div.biartpost:nth-child(3) > ul:nth-child(2)";
         const ul = await this.page?.waitForSelector(SELECTOR);
-        const urls = (await ul?.$$("li"))!.map(async li => {
+        let temp = (await ul?.$$("li"))!.map(async li => {
             const music: IMusic = {
                 name: "",
                 url: ""
             };
             try {
-                music.name = (await((await (await li!.$("strong"))!.getProperty("textContent"))!.jsonValue()))!;
-                music.url = (await((await (await li!.$("a"))!.getProperty("href"))!.jsonValue()))!;
-            } catch (err){
+                music.name = (await ((await (await li!.$("strong"))!.getProperty("textContent"))!.jsonValue()))!;
+                music.url = (await ((await (await li!.$("a"))!.getProperty("href"))!.jsonValue()))!;
+            } catch (err) {
                 if (err instanceof Error)
                     console.error(err.message);
             }
             return music;
         });
-        this.musicUrls.single_musics = await Promise.all(urls);
+        const urls = await Promise.all(temp);
+        this.musicUrls.single_musics = await this.findSingleMusicsDownloadUrl(urls);
     }
 
+    private async findSingleMusicsDownloadUrl(urls: IMusic[]) {
+        const temp = [];
+        for (let i = 0; i < BiaMusic.MAX_OPEN_TAB; i++) {
+            temp.push(BiaMusic.browser.newPage());
+        }
+        const pages = await Promise.all(temp);
+        let queue = [];
+        console.log(urls.length);
+        for (let i = 0; i < urls.length; i+=BiaMusic.MAX_OPEN_TAB) {
+            queue = pages.map(async (page, index) => {
+                try {
+                    if (i + index >= urls.length)
+                        return;
+                    console.log("-------------\n", i + index, "\n------------------")
+                    await page.goto(urls[i + index].url, BiaMusic.GO_TO_OPTIONS);
+                    let url;
+                    if (this.quality === Quality._320) {
+                        const QUALITY_320_SELECTOR = ".biadl > a:nth-child(3)";
+                        url = await (await (await page.waitForSelector(QUALITY_320_SELECTOR))!.getProperty("href")).jsonValue();
+                    } else {
+                        const QUALITY_128_SELECTOR = ".biadl > a:nth-child(4)";
+                        url = await (await (await page.waitForSelector(QUALITY_128_SELECTOR))!.getProperty("href")).jsonValue();
+                    }
+                    urls[i + index].url = url;
+                } catch (err) {
+                    console.log(i + index);
+                    if (err instanceof Error)
+                        console.error(err.message);
+                }
+            });
+            await Promise.all(queue);
+        }
+
+        return urls;
+    }
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import BiaMusic from "./biaMusic.js";
+import BiaMusic, {Quality} from "./biaMusic.js";
 
 (async () => {
-    const obj = new BiaMusic("وحید تاج", "https://biamusic.ir/artist/vahid-taj/");
+    const obj = new BiaMusic("وحید تاج", "https://biamusic.ir/artist/vahid-taj/", Quality._320);
     await obj.find();
 })();
